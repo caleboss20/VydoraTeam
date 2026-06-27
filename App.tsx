2f04f020d800c7a5d5c28935bf923f76f623c80b
caleboss <philanthropistcal@gmail.com>
2026-06-27 13:39:11 +0000

    feat:Add context provider in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,48 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import MainStackNavigator from "./src/screens/MainstackNavigator"
 import { NavigationContainer } from '@react-navigation/native';
-
-// transactionprovider as using for state management
-// for transactions data to made available to all screens//
-// any screen can read or add transactionswithout passing props
+import { AuthProvider } from './src/screens/Contexts/Authcontext'; 
+import { ProjectProvider } from './src/screens/Contexts/projectContext'; 
+import { ClipProvider } from './src/screens/Contexts/clipContext'; 
+import { MemberProvider } from './src/screens/Contexts/memberContext'; 
+import { CommentProvider } from './src/screens/Contexts/commentContext'; 
+import { NotificationProvider } from './src/screens/Contexts/notificatinContext'; 
 
 export default function App() {
   return (
-
-       <NavigationContainer>
-    <MainStackNavigator />
-   </NavigationContainer>
-    
+    <SafeAreaProvider>
+      <AuthProvider>
+        <ProjectProvider>
+          <ClipProvider>
+            <MemberProvider>
+              <CommentProvider>
+                <NotificationProvider>
+                  <NavigationContainer>
+                    <MainStackNavigator />
+                  </NavigationContainer>
+                </NotificationProvider>
+              </CommentProvider>
+            </MemberProvider>
+          </ClipProvider>
+        </ProjectProvider>
+      </AuthProvider>
+    </SafeAreaProvider>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
